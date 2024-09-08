0c738d5cbe370612f01e621aaeae12322c2d14f4
Estebans441 <salazaraesteban@javeriana.edu.co>
2024-09-07 20:09:27 -0500

    feat: connection class

---
+++ FrontSUB/src/backend/matchEventService.ts
@@ -0,0 +1,48 @@
+
+import { Client } from '@stomp/stompjs';
+import { type Match, type Event } from "../types/match";
+
+export default class MatchEventService {
+  client: Client;
+  match: Match;
+
+  constructor(eventTypes: string[], match: Match, setEvents: (events: (prevEvents: Event[]) => Event[]) => void) {
+    this.match = match;  
+    this.client = new Client({
+        brokerURL: 'ws://localhost:15674/ws', // URL del WebSocket de RabbitMQ
+        connectHeaders: {
+            login: 'guest',
+            passcode: 'guest',
+        },
+        debug: (str) => {
+            console.log(str);
+        },
+        onStompError: (frame) => {
+            console.error('Error de STOMP:', frame.headers['message']);
+        },
+    });
+  }
+
+  subscribe(eventType: string, setEvents: (events: (prevEvents: Event[]) => Event[]) => void) {
+    this.client.subscribe(`/exchange/match_events/match.${this.match.id}.event.${eventType}`, (message) => {
+      console.log('Mensaje recibido:', message.body);
+      setEvents((prevEvents: Event[]) => [...prevEvents, JSON.parse(message.body)]);
+    });
+  }
+
+  subscribeToNewTopic(eventType: string, setEvents: (events: (prevEvents: Event[]) => Event[]) => void) {
+    if (this.client.connected) {
+      this.subscribe(eventType, setEvents);
+    } else {
+      console.error('No se puede suscribir, el cliente no está conectado.');
+    }
+  }
+
+  activate() {
+    this.client.activate();
+  }
+
+  deactivate() {
+    this.client.deactivate();
+  }
+}
--- FrontSUB/src/components/events/ListEvents.tsx
+++ FrontSUB/src/components/events/ListEvents.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Client } from '@stomp/stompjs';
 import { type Match, type Event } from "../../types/match";
 import MatchEventCard from './MatchEventCard';
+import MatchEventService from '../../backend/matchEventService';
 
 interface Props {
   match: Match;
@@ -9,37 +9,20 @@ interface Props {
 
 const ListEvents: React.FC<Props> = ({ match }) => {
   const [events, setEvents] = useState<Event[]>([]);
-  const [eventTypes, setEventTypes] = useState<string[]>(["goal", "penalty"]);
+  const [eventTypes, setEventTypes] = useState<string[]>(["#"]);
   const localTeamId = match.homeTeam;
+  const matchEventService = new MatchEventService(eventTypes, match, setEvents);
 
   useEffect(() => {
-    const client = new Client({
-        brokerURL: 'ws://localhost:15674/ws', // URL del WebSocket de RabbitMQ
-        connectHeaders: {
-            login: 'guest',
-            passcode: 'guest',
-        },
-        debug: (str) => {
-            console.log(str);
-        },
-        onConnect: () => {
-          for (let eventType of eventTypes) {
-            console.log('Conectado a RabbitMQ WebSocket');
-            client.subscribe(`/exchange/match_events/match.${match.id}.event.${eventType}`, (message) => {
-              console.log('Mensaje recibido:', message.body);
-              setEvents((prevEvents) => [...prevEvents, JSON.parse(message.body)]);
-            });
-          }
-        },
-        onStompError: (frame) => {
-            console.error('Error de STOMP:', frame.headers['message']);
-        },
-    });
-
-    client.activate();
-
+    matchEventService.activate();
+    matchEventService.client.onConnect = () => {
+      console.log('Conectado a RabbitMQ WebSocket');
+      for (let eventType of eventTypes) {
+        matchEventService.subscribe(eventType, setEvents);
+      }
+    };
     return () => {
-        client.deactivate();
+        matchEventService.deactivate();
     };
 }, []);
 
