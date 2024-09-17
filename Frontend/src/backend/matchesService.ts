a0f960f764d6d1590f618d84feecb94da535f127
juanfra312003 <juanframireze@javeriana.edu.co>
2024-09-17 12:05:25 -0500

    Feature: Change Ports

--- Frontend/src/backend/matchesService.ts
+++ Frontend/src/backend/matchesService.ts
@@ -4,7 +4,7 @@ const BACKEND_HOST = process.env.BACKEND_HOST || 'localhost';
 const RABBITMQ_HOST = process.env.RABBITMQ_HOST || 'localhost';
 
 export const getMatches = async (): Promise<Match[]> => {
-    const response = await fetch(`http://${BACKEND_HOST}:8081/matches`);
+    const response = await fetch(`http://${BACKEND_HOST}:8080/matches`);
     const matches = await response.json();
     return matches;
 }
